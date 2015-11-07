7d4513a35e584e7a2477c9b11e8fb7222e988b4b
Norman <ua.norman@gmail.com>
2015-11-07 14:45:00 +0100

    Test about relationship between sports and establishments, were moved to establishments-sports-test

--- routes/sports.js
+++ routes/sports.js
@@ -102,6 +102,143 @@ router.get('/:id', function(req, res) {
   }
 });
 
+router.get('/:id/establishments', function(req, res) {
+  if(req.query.after){
+    if(req.query.limit){
+      models.sport.findOne({where:{id:req.params.id}}).then(function(sport){
+        if (sport == undefined)
+          res.status(404).send({message: "The sport was not found"});
+        else{
+          if(req.query.limit == 0)
+            res.status(400).send({message: 'The limit for pagination, must be greater than 0'})
+          else {
+            var after = parseInt(new Buffer(req.query.after, 'base64').toString('ascii'));
+            sport.getEstablishments({attributes: ['id', 'name', 'desc', 'city', 'province', 'addr','phone', 'website','main_img'],
+              joinTableAttributes: [], limit: limit, include: [{model: models.user, as: 'Owner',
+                attributes: ['uuid','name','lname','email','gender']}],where: {id: {$gt: after}}
+            }).then(function (ests) {
+              var before = new Buffer(ests[0].id.toString()).toString('base64');
+              sport.getEstablishments().then(function (count) {
+                var after = 0;
+                var next = 'none';
+                if (ests[ests.length - 1].id < count[count.length - 1].id && count.length > req.query.limit) {
+                  after = new Buffer(ests[ests.length - 1].id.toString()).toString('base64');
+                  next = req.protocol + "://" + req.hostname + ":3000" + "/api/sports/" + req.params.id +
+                      "/establishments?after=" + after + '&limit=' + req.query.limit;
+                }
+                var curs = {before: before, after: after};
+                var prev = req.protocol + "://" + req.hostname + ":3000" + "/api/sports/" + req.params.id +
+                    "/establishments?before=" + before + '&limit=' + req.query.limit;
+                var pag = {cursors: curs, previous: prev, next: next};
+                res.status(200).send({
+                  Establishmets: ests, paging: pag, links: {
+                    rel: 'self', href: req.protocol + "://" + req.hostname + ":3000" +
+                    "/api/sports" + req.params.id + "/establishments"
+                  }
+                });
+              })
+            })
+          }
+        }
+      })
+    }
+    else
+      res.status(400).send({message: "Wrong parameters, limit parameter must be set for paging"});
+  }
+  else if(req.query.before){
+    if(req.query.limit){
+      models.sport.findOne({where:{id:req.params.id}}).then(function(sport) {
+        if (sport == undefined)
+          res.status(404).send({message: "The sport was not found"});
+        else {
+          if(req.query.limit == 0)
+            res.status(400).send({message: 'The limit for pagination, must be greater than 0'})
+          else {
+            var before = parseInt(new Buffer(req.query.before, 'base64').toString('ascii'));
+            sport.getEstablishments({attributes: ['id', 'name', 'desc', 'city', 'province', 'addr','phone', 'website','main_img'],
+              joinTableAttributes: [], limit: limit, include: [{model: models.user, as: 'Owner',
+                attributes: ['uuid','name','lname','email','gender']}],where: {id: {$lt: before}}
+            }).then(function (ests) {
+              sport.getEstablishments().then(function (count) {
+                var after = new Buffer(ests[ests.length - 1].id.toString()).toString('base64');
+                var next = req.protocol + "://" + req.hostname + ":3000" + "/api/sports/" +
+                    req.params.id + "/establishments?after=" + after + '&limit=' + req.query.limit;
+                var before = 0;
+                var prev = 'none';
+                //In this case, there is no need to lookup the total value of the sports
+                if (ests[0].id > count[0].id) {
+                  before = new Buffer(ests[0].id.toString()).toString('base64');
+                  prev = req.protocol + "://" + req.hostname + ":3000" + "/api/sports/" + req.params.id +
+                      "/establishments?before=" + before + '&limit=' + req.query.limit;
+                }
+                var curs = {before: before, after: after};
+                var pag = {cursors: curs, previous: prev, next: next};
+                res.status(200).send({
+                  Establishments: ests, paging: pag, links: {
+                    rel: 'self',
+                    href: req.protocol + "://" + req.hostname + ":3000" + "/api/sports/" + req.params.id + "/establishments"
+                  }
+                });
+              })
+            })
+          }
+        }
+      })
+    }
+    else
+      res.status(400).send({message: "Wrong parameters, limit parameter must be set for paging"});
+  }
+  else {
+    var limit = 5;
+    if(req.query.limit)
+      limit=req.query.limit;
+    //Checking that establishment exists
+    models.sport.findOne({where:{id:req.params.id}}).then(function(sport) {
+      if (sport == undefined)
+        res.status(404).send({message: "The sport was not found"});
+      else {
+        //If establishment exists, we retrieve the sports
+        if(limit == 0)
+          res.status(400).send({message: 'The limit for pagination, must be greater than 0'})
+        else {
+          sport.getEstablishments({attributes: ['id', 'name', 'desc', 'city', 'province', 'addr','phone', 'website','main_img'],
+            joinTableAttributes: [], limit: limit, include: [{model: models.user, as: 'Owner',
+              attributes: ['uuid','name','lname','email','gender']}]}).then(function (ests) {
+            var before = 0;
+            var after = 0;
+            var next = 'none';
+            if(ests.length == 0)
+              res.status(200).send(sports);
+            else {
+              //We get the total number of the sports
+              sport.getEstablishments().then(function (count) {
+                //If last item retrieved is lower than max id and the number of total sports is greater than the limit
+                //Then we'll have after cursors
+                if (ests[ests.length - 1].id < count[count.length - 1].id && count.length > limit) {
+                  after = new Buffer(ests[ests.length - 1].id.toString()).toString('base64');
+                  next = req.protocol + "://" + req.hostname + ":3000" + "/api/sports/" + req.params.id +
+                      "/establishments?after=" + after + '&limit=' + limit;
+                }
+                var curs = {before: before, after: after};
+                var prev = 'none';
+                var pag = {cursors: curs, previous: prev, next: next};
+                res.status(200).send({
+                  Establishments: ests,
+                  paging: pag,
+                  links: {
+                    rel: 'self',
+                    href: req.protocol + "://" + req.hostname + ":3000" + "/api/sports/" + req.params.id + "/establishments"
+                  }
+                });
+              })
+            }
+          })
+        }
+      }
+    })
+  }
+});
+
 router.post('/new', authController.isBearerAuthenticated, function(req, res) {
   if(models.user.isOwner(req.get('Authorization').slice('7'))){
     if (req.body.name) {
