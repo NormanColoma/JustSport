49bdea67eb5b705a871b4179cf860bbd7a1c6ab0
Norman <ua.norman@gmail.com>
2015-11-09 20:25:31 +0100

    TDD: Getting all establishment filtered by sport and location. FAIL

--- seeders/test/20151108194604-course-test-seeder.js
+++ seeders/test/20151108194604-course-test-seeder.js
@@ -2,9 +2,11 @@
 var course1 = {sportId:'1', establishmentId:'1',instructor: 'Juan Domínguez',price:'17.50',info:'Un curso muy completo'};
 var course2 = {sportId:'2', establishmentId:'1',instructor: 'Pepe Castaño',price:'20',info:'Un curso no tan completo'};
 var course3 = {sportId:'3', establishmentId:'1',instructor: 'María Castro',price:'15',info:'Un curso poco completo'};
+var course4 = {sportId:'1', establishmentId:'2',instructor: 'Pili Fernández',price:'25',info:'El curso es muy completo'};
+var course5 = {sportId:'1', establishmentId:'4',instructor: 'Ana Fernández',price:'22.75',info:'El curso es para experimentado'};
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('courses',[course1,course2,course3]);
+    return queryInterface.bulkInsert('courses',[course1,course2,course3,course4,course5]);
   },
 
   down: function (queryInterface, Sequelize) {
