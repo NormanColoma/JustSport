f76786a3cd9167849456969a5039c6b73f38b197
Norman <ua.norman@gmail.com>
2015-11-13 14:59:53 +0100

    pending

---
+++ migrations/20151113141451-create-schedule.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('schedules', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      day: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      time: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      courseId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: "courses", key: "id"},
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('schedules');
+  }
+};
---
+++ routes/schedules.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Norman on 13/11/2015.
+ */
+/**
+ * Created by Norman on 08/11/2015.
+ */
+var models  = require('../models');
+var express = require('express');
+var router  = express.Router();
+var authController = require('../routes/auth');
+var user = require('../middlewares/checkUser');
+var middleware = require('../middlewares/paramMiddleware');
+
+router.post('/new', authController.isBearerAuthenticated, user.isEstabOwner2, function(req, res) {
+    if (req.body.day && req.body.time && req.body.courseId) {
+        models.schedule.create(req.body).then(function (schedule) {
+            var url = req.protocol + "://" + req.hostname + ":" + global.port + "/api/schedules/" + schedule.id;
+            res.setHeader("Location", url);
+            var links = new Array();
+            var link1 = {
+                rel: 'self',
+                href: req.protocol + "://" + req.hostname + ":" + global.port + "/api/schedules/new"
+            };
+            var link2 = {
+                rel: 'update',
+                href: req.protocol + "://" + req.hostname + ":" + global.port + "/api/schedules/" + schedule.id
+            };
+            var link3 = {
+                rel: 'delete',
+                href: req.protocol + "://" + req.hostname + ":" + global.port + "/api/schedules/" + schedule.id
+            };
+            links.push([link1, link2, link3]);
+            res.status(201).send({
+                id: schedule.id, day: schedule.day, time: schedule.time, courseId: schedule.courseId, links: links
+            });
+        }).catch(function (err) {
+            console.log(err);
+            res.status(500).send(err);
+        })
+
+    }
+    else
+        res.status(400).send({message: "Json is malformed, it must include the following fields: day,time, courseId"});
+
+});
+
+module.exports = router;
