534dd6ab239c3c3bc90e3daab1bfee69fd6b6d18
Norman <ua.norman@gmail.com>
2015-11-18 20:04:35 +0100

    Port variable was added to start the server

--- routes/sports.js
+++ routes/sports.js
@@ -47,7 +47,7 @@ router.get('', function(req, res) {
             res.status(200).send({sports:sports,paging:pag,links: {rel:'self',href:req.protocol + "://" + req.hostname + ":3000" + "/api/sports"}});
           })
         }).catch(function(err){
-          console.log(err)
+          console.log(req)
           res.status(500).send(err);
         })
       }
@@ -73,7 +73,7 @@ router.get('', function(req, res) {
           res.status(200).send({sports: sports, paging: pag,links: {rel:'self',href:req.protocol + "://" + req.hostname + ":3000" + "/api/sports"}});
         })
       }).catch(function (err) {
-        res.status(500).send("No se puede conectar "+req.hostname);
+        res.status(500).send(err);
       })
     }
 });
