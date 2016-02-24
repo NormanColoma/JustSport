4277297ddeda181d1f1b5beef8545e5f9aa35ad4
Norman <ua.norman@gmail.com>
2016-02-24 09:12:59 +0100

    MochaTest as grunt task, was added

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,22 @@ module.exports = function(grunt){
         watch: {
             files: ['<%= jshint.files %>'],
             tasks: ['jshint']
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: 'results.txt', // Optionally capture the reporter output to a file
+                    quiet: false, // Optionally suppress output to standard out (defaults to false)
+                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+                },
+                src: ['test/**/*.js']
+            }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['jshint']);
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.registerTask('default', ['jshint', 'mochaTest']);
 
 };
