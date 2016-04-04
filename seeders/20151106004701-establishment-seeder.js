7baadab142a35c7df66f9af3df6616c2314553a3
Norman <ua.norman@gmail.com>
2016-04-04 19:46:52 +0200

    Seeder were changed

--- seeders/20151106004701-establishment-seeder.js
+++ seeders/20151106004701-establishment-seeder.js
@@ -1,20 +1,40 @@
 'use strict';
 var owner_id = '8a74a3aa-757d-46f1-ba86-a56a0f107735';
-var est1 = {name: 'Gym A Tope', desc: 'Gimnasio perfecto para realizar tus actividades deportivas.',
+var est1 = {name: 'Gym A Tope', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ' +
+'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ' +
+'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
+'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia ' +
+'deserunt mollit anim id est laborum',
   city: 'San Vicente del Raspeig', province: 'Alicante', addr: 'Calle San Franciso nº15',
-  phone: '965660327', website: 'http://wwww.gymatope.es', main_img:'atope.jpeg',owner: owner_id};
-var est2 = {name: 'Gym Noray', desc: 'Gimnasio muy acondicionado y en perfecto estado.',
+  phone: '965660327', website: 'http://wwww.gymatope.es', main_img:'default.jpg',owner: owner_id};
+var est2 = {name: 'Gym Noray', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ' +
+'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ' +
+'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
+'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia ' +
+'deserunt mollit anim id est laborum',
   city: 'Santa Pola', province: 'Alicante', addr: 'Calle Falsa nº34',
-  phone: '965662347', website: 'http://wwww.noraygym.com', main_img:'noray.jpeg',owner: owner_id};
-var est3 = {name: 'Más Sport', desc: 'Asociación deportiva con unas instalaciones increíbles.',
+  phone: '965662347', website: 'http://wwww.noraygym.com', main_img:'default.jpg',owner: owner_id};
+var est3 = {name: 'Más Sport', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ' +
+'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ' +
+'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
+'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia ' +
+'deserunt mollit anim id est laborum',
   city: 'Valencia', province: 'Valencia', addr: 'Calle Arco nº32',
-  phone: '965663057', website: 'http://wwww.masport.es', main_img:'mas.jpeg',owner: owner_id};
-var est4 = {name: 'Montemar', desc: 'Especializados en cursos y clases de ténis.',
+  phone: '965663057', website: 'http://wwww.masport.es', main_img:'crossfit.jpg',owner: owner_id};
+var est4 = {name: 'Montemar', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ' +
+'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ' +
+'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
+'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia ' +
+'deserunt mollit anim id est laborum',
   city: 'Alicante', province: 'Alicante', addr: 'Avenida Novelda Km 14',
-  phone: '965662268', website: 'http://wwww.montemar.es', main_img:'montemar.jpeg',owner: owner_id};
-var est5 = {name: 'Gimnasio 13', desc: 'El mejor lugar para ponerte en forma.',
+  phone: '965662268', website: 'http://wwww.montemar.es', main_img:'default.jpg',owner: owner_id};
+var est5 = {name: 'Gimnasio 13', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ' +
+'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ' +
+'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
+'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia ' +
+'deserunt mollit anim id est laborum',
   city: 'Barcelona', province: 'Barcelona', addr: 'Gran Vía nº15',
-  phone: '965662257', website: 'http://wwww.13gym.es', main_img:'13gym.jpeg',owner: owner_id};
+  phone: '965662257', website: 'http://wwww.13gym.es', main_img:'gym-1.jpg',owner: owner_id};
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
