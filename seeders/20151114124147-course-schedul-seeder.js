df60c61581661c1603f1fe35db53683e4c259061
Norman <ua.norman@gmail.com>
2016-04-22 14:05:07 +0200

    Full schedule route was added

--- seeders/20151114124147-course-schedul-seeder.js
+++ seeders/20151114124147-course-schedul-seeder.js
@@ -5,9 +5,12 @@ var s3 = {day: 'Miércoles', startTime: '17:00', endTime:"18:00", courseId: 1};
 var s5 = {day: 'Jueves', startTime: '20:00', endTime:"21:00", courseId: 1};
 var s4 = {day: 'Jueves', startTime: '12:00', endTime:"13:00", courseId: 1};
 var s6 = {day: 'Viernes', startTime: '09:00', endTime:"10:00", courseId: 1};
+var s7 = {day: 'Jueves', startTime: '12:30', endTime:"13:30", courseId: 2};
+var s8 = {day: 'Jueves', startTime: '20:00', endTime:"21:00", courseId: 2};
+var s9 = {day: 'Viernes', startTime: '10:00', endTime:"11:00", courseId: 2};
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('schedules',[s1,s2,s3,s4,s5,s6]);
+    return queryInterface.bulkInsert('schedules',[s1,s2,s3,s4,s5,s6,s7,s8,s9]);
   },
 
   down: function (queryInterface, Sequelize) {
