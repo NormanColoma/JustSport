07886042c3d87a6f9a37450fb34a6884dbda5487
Norman <ua.norman@gmail.com>
2016-06-08 18:49:19 +0200

    References for primrary key of establishmentsports table were changed

--- seeders/20151106004701-establishment-seeder.js
+++ seeders/20151106004701-establishment-seeder.js
@@ -6,14 +6,14 @@ var est1 = {name: 'Gym A Tope', desc: 'Lorem ipsum dolor sit amet, consectetur a
 'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia ' +
 'deserunt mollit anim id est laborum',
   city: 'San Vicente del Raspeig', province: 'Alicante', addr: 'Calle San Franciso nº15',
-  phone: '965660327', website: 'http://wwww.gymatope.es', main_img:'default.jpg',owner: owner_id};
+  phone: '965660327', website: 'http://wwww.gymatope.es', main_img:'gym-1.jpg',owner: owner_id};
 var est2 = {name: 'Gym Noray', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ' +
 'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ' +
 'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
 'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia ' +
 'deserunt mollit anim id est laborum',
   city: 'Santa Pola', province: 'Alicante', addr: 'Calle Falsa nº34',
-  phone: '965662347', website: 'http://wwww.noraygym.com', main_img:'default.jpg',owner: owner_id};
+  phone: '965662347', website: 'http://wwww.noraygym.com', main_img:'gimnasios-privados3.jpg',owner: owner_id};
 var est3 = {name: 'Más Sport', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ' +
 'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ' +
 'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
@@ -27,7 +27,7 @@ var est4 = {name: 'Montemar', desc: 'Lorem ipsum dolor sit amet, consectetur adi
 'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia ' +
 'deserunt mollit anim id est laborum',
   city: 'Alicante', province: 'Alicante', addr: 'Avenida Novelda Km 14',
-  phone: '965662268', website: 'http://wwww.montemar.es', main_img:'default.jpg',owner: owner_id};
+  phone: '965662268', website: 'http://wwww.montemar.es', main_img:'spa-gym-malecon-zarautz-recepcion2-.jpg',owner: owner_id};
 var est5 = {name: 'Gimnasio 13', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ' +
 'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ' +
 'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
